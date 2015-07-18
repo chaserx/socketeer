ab49e7a3ed12f8a8cb5379d57287c82f6adf83ee
Chase Southard <chase.southard@gmail.com>
2015-07-18 17:16:13 -0400

    gulpfile for live reload; index.html

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+var notify = require('gulp-notify');
+var livereload = require('gulp-livereload');
+var jshint = require('gulp-jshint');
+
+// Tasks
+gulp.task('jshint', function() {
+  return gulp.src('app.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('app.js', ['jshint']);
+});
+
+gulp.task('liveserver', function() {
+  // listen for changes
+  livereload.listen();
+  // configure nodemon
+  nodemon({
+    // the script to run the app
+    script: 'app.js',
+    ext: 'js'
+  }).on('restart', function(){
+    // when the app has restarted, run livereload.
+    gulp.src('app.js')
+      .pipe(livereload())
+      .pipe(notify('Reloading page, please wait...'));
+  })
+});
+
+gulp.task('default', ['watch', 'liveserver']);
