812a4179cb26c493157534b0705f4a6a714df99b
nikolaiwarner <n@nikolaiwarner.com>
2015-07-19 18:53:16 -0400

    add a procfile for heroku

--- app.js
+++ app.js
@@ -67,6 +67,7 @@ function respond(msg) {
   }
 }
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+var port = process.env.PORT || 3000;
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
